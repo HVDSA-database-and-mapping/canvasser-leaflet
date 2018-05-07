458cdefd62830ee0ee10dea7778c2abe2ae7e0b9
Will Turner <rwturner17@gmail.com>
2018-05-07 19:48:39 -0400

    Add form for new canvas

--- canvasser/forms.py
+++ canvasser/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class NewCanvasForm(forms.Form):
-    new_canvas_name = forms.CharField(label='Name your new canvas',
-        max_length=140)
+    new_canvas_name = forms.CharField(label='new_canvas_form',
+        max_length=140, help_text='140 characters max')
 
--- canvasser/views.py
+++ canvasser/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 from .models import Canvas, Canvasser
 from djgeojson.views import TiledGeoJSONLayerView
+
+import datetime
+
+from .forms import NewCanvasForm
 
 
 class UntrimmedTiledGeoJSONLayerView(TiledGeoJSONLayerView):
@@ -16,3 +22,26 @@ class CanvasListView(generic.ListView):
 
 class CanvasserListView(generic.ListView):
     model = Canvasser
+
+
+def index(request):
+
+    if request.method == 'POST':
+        form = NewCanvasForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # (here we just write it to the model due_back field)
+            canvas_inst = Canvas(name=form.cleaned_data['new_canvas_name'])
+            canvas_inst.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('canvases'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        form = NewCanvasForm()
+
+    return render(request, 'canvasser/index.html',
+        {'form': form})
