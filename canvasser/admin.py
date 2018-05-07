a1f718e555015d1f95cc550b5166f683543de1e2
Will Turner <rwturner17@gmail.com>
2018-05-07 02:15:41 +0000

    Add canvas models to admin

--- canvasser/admin.py
+++ canvasser/admin.py
@@ -1,8 +1,12 @@
 from django.contrib.gis import admin
-from .models import CensusTract, CongressionalDistrict, County, Parcel 
+from .models import CensusTract, CongressionalDistrict, County, Parcel, Canvas, Canvasser, CanvasArea, CanvasSector
 
 admin.site.register(CensusTract, admin.OSMGeoAdmin)
 admin.site.register(CongressionalDistrict, admin.OSMGeoAdmin)
 admin.site.register(County, admin.OSMGeoAdmin)
 admin.site.register(Parcel, admin.OSMGeoAdmin)
+admin.site.register(Canvas)
+admin.site.register(Canvasser)
+admin.site.register(CanvasArea, admin.OSMGeoAdmin)
+admin.site.register(CanvasSector, admin.OSMGeoAdmin)
 
