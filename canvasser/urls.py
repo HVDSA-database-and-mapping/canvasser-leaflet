13323a6607e61e8462b18497602dcdedc469e32f
Will Turner <rwturner17@gmail.com>
2018-05-07 19:53:10 -0400

    Add urlmap to index

--- canvasser/urls.py
+++ canvasser/urls.py
@@ -2,11 +2,10 @@ from django.urls import path, re_path
 from django.views.generic import TemplateView
 from .models import Parcel
 from .views import UntrimmedTiledGeoJSONLayerView, CanvasListView, \
-    CanvasserListView
+    CanvasserListView, index
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='canvasser/index.html'),
-        name='index'),
+    path('', index, name='index'),
     path('canvases/', CanvasListView.as_view(), name='canvases'),
     path('canvassers/', CanvasserListView.as_view(), name='canvassers'),
     re_path(r'^data/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+).geojson$',
