89b692f18bf835e82310ad1496a9a3e330c4b83c
Will Turner <rwturner17@gmail.com>
2018-05-19 18:00:35 +0000

    Remove geographical entities from admin

--- canvasser/admin.py
+++ canvasser/admin.py
@@ -1,10 +1,6 @@
 from django.contrib.gis import admin
 from .models import *
 
-admin.site.register(CensusTract, admin.OSMGeoAdmin)
-admin.site.register(CongressionalDistrict, admin.OSMGeoAdmin)
-admin.site.register(County, admin.OSMGeoAdmin)
-admin.site.register(Parcel, admin.OSMGeoAdmin)
 admin.site.register(Canvas)
 admin.site.register(Canvasser)
 admin.site.register(CanvasArea, admin.OSMGeoAdmin)
